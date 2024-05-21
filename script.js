3bed7a198b356a394efd55e561b1018ccef833a9
eshan-sud <eshansud22@gmail.com>
2024-05-21 22:28:02 +0530

    The canvas element doesn't reset after downloading or changing backgrounds now

--- script.js
+++ script.js
@@ -1,112 +1,67 @@
 window.addEventListener("load", () => {
     resize();
-    document.addEventListener("mousedown", startPainting);
-    document.addEventListener("mouseup", stopPainting);
-    document.addEventListener("mousemove", sketch);
-    document.addEventListener("touchstart", startTouchPainting, { passive: false });
-    document.addEventListener("touchend", stopPainting);
-    document.addEventListener("touchmove", touchSketch, { passive: false });
     window.addEventListener("resize", resize);
+    document.addEventListener("mouseup", stopPainting);
+    document.addEventListener("mousedown", startPainting);
+    document.addEventListener("mousemove", sketch);
+    document.addEventListener("touchend", stopPainting);
+    document.addEventListener("touchstart", startTouchPainting, { passive: false });
+    document.addEventListener("touchmove", touchSketch, { passive: false });
+    backgroundColorSetting.addEventListener("click", changeBackgroundTransparency);
     document.getElementById("toolbar-settings").addEventListener("change", updateToolbarSettings);
-    document.getElementById("background-check").addEventListener("click", changeBackgroundTransparency);
 });
 // Drawing Board
 const drawingBoard = document.getElementById("drawing-board");
-const context = drawingBoard.getContext("2d");
-let color;
-let strokeWidth = 5, strokeColor = "black";
+const backgroundBoard = document.getElementById("background-board");
+const drawingContext = drawingBoard.getContext("2d");
+const backgroundContext = backgroundBoard.getContext("2d");
+const backgroundColorSetting = document.getElementById("background-check");
+let backgroundColor = "white", strokeWidth = 5, strokeColor = "black";
 let coords = { x: 0, y: 0 };
 let paint = false;
 // Update the Toolbar Settings
 function updateToolbarSettings() {
     strokeWidth = document.getElementById("stroke-width").value;
     strokeColor = document.getElementById("stroke-color").value;
-    if (!document.getElementById("background-check").checked) {
-        color = document.getElementById("background-color").value;
-        context.save();
-        context.fillStyle = color;
-        context.fillRect(0, 0, drawingBoard.width, drawingBoard.height);
-        context.restore();
+    backgroundColor = document.getElementById("background-color").value;
+    if (backgroundColorSetting.checked) {
+        clearBackground();
+        return;
     }
-}
-// Clears the Canvas
-function clearCanvas() {
-    paint = false;
-    context.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
-    updateToolbarSettings();
-}
-// Saves the Canvas
-// function save() {
-//     const documentSelection = document.getElementById("download-options").value;
-//     const selection = {
-//         "png": "image/png",
-//         "jpg": "image/jpg",
-//         "pdf": "application/pdf"
-//     };
-//     const dataURL = drawingBoard.toDataURL(selection[documentSelection]);
-//     let downloadButton = document.getElementById("download-button").querySelector("a");
-//     downloadButton.href = dataURL;
-//     downloadButton.download = "signature." + documentSelection;
-// }
-function save() {
-    const documentSelection = document.getElementById("download-options").value;
-    switch (documentSelection) {
-        case "png":
-        case "jpg":
-            saveImage(documentSelection);
-            break;
-        case "pdf":
-            savePDF();
-            break;
-        default:
-            console.error("Unsupported file format selected");
-    }
-}
-function saveImage(format) {
-    const dataURL = drawingBoard.toDataURL(`image/${format}`);
-    const link = document.createElement("a");
-    link.href = dataURL;
-    link.download = `signature_${getTimestamp()}.${format}`;
-    link.click();
-}
-const { jsPDF } = window.jspdf;
-function savePDF() {
-    const canvas = document.getElementById("drawing-board");
-    const imgData = canvas.toDataURL("image/jpg", 1.0);
-    const pdf = new jsPDF();
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
-    const pageWidth = pdf.internal.pageSize.getWidth();
-    const pageHeight = pdf.internal.pageSize.getHeight();
-    const ratio = Math.min(pageWidth / canvasWidth, pageHeight / canvasHeight);
-    const width = canvasWidth * ratio;
-    const height = canvasHeight * ratio;
-    const x = (pageWidth - width) / 2;
-    const y = (pageHeight - height) / 2;
-    pdf.addImage(imgData, 'JPG', x, y, width, height);
-    const format = "pdf";
-    pdf.save(`signature_${getTimestamp()}.${format}`);
-}
-function getTimestamp() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+    backgroundContext.save();
+    backgroundContext.fillStyle = backgroundColor;
+    backgroundContext.fillRect(0, 0, drawingBoard.width, drawingBoard.height);
+    backgroundContext.restore();
 }
 // Background Color Settings
 function changeBackgroundTransparency() {
     const backgroundRow = document.getElementById("background-color-row");
     backgroundRow.style.visibility = backgroundRow.style.visibility === "hidden" ? "visible" : "hidden";
-    clearCanvas();
+    backgroundColorSetting.checked ? clearBackground() : updateToolbarSettings();
+}
+// Clears both the Canvases
+function clearCanvas() {
+    paint = false;
+    drawingContext.save();
+    backgroundContext.save();
+    drawingContext.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+    backgroundContext.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+    drawingContext.restore();
+    backgroundContext.restore();
+    updateToolbarSettings();
+}
+// Clears the Background
+function clearBackground() {
+    backgroundContext.save();
+    backgroundContext.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+    backgroundContext.restore();
 }
 // Resizes the Canvas
 function resize() {
-    context.canvas.width = 750;
-    context.canvas.height = 250;
+    drawingContext.canvas.width = 750;
+    drawingContext.canvas.height = 250;
+    backgroundContext.canvas.width = 750;
+    backgroundContext.canvas.height = 250;
     updateToolbarSettings();
 }
 // Gets the position of the Cursor
@@ -137,33 +92,94 @@ function startTouchPainting(event) {
         getTouchPosition(event);
     }
 }
-// Stops Painting
-function stopPainting() {
-    paint = false;
-}
 // Sketchs the Drawing
 function sketch(event) {
     if (!paint) return;
-    context.beginPath();
-    context.strokeStyle = strokeColor;
-    context.lineWidth = strokeWidth;
-    context.lineCap = "round";
-    context.moveTo(coords.x, coords.y);
+    drawingContext.beginPath();
+    drawingContext.strokeStyle = strokeColor;
+    drawingContext.lineWidth = strokeWidth;
+    drawingContext.lineCap = "round";
+    drawingContext.moveTo(coords.x, coords.y);
     getPosition(event);
-    context.lineTo(coords.x, coords.y);
-    context.stroke();
+    drawingContext.lineTo(coords.x, coords.y);
+    drawingContext.stroke();
 }
 function touchSketch(event) {
     if (!paint) return;
     if (event.target === drawingBoard) {
         event.preventDefault();
-        context.beginPath();
-        context.strokeStyle = strokeColor;
-        context.lineWidth = strokeWidth;
-        context.lineCap = "round";
-        context.moveTo(coords.x, coords.y);
+        drawingContext.beginPath();
+        drawingContext.strokeStyle = strokeColor;
+        drawingContext.lineWidth = strokeWidth;
+        drawingContext.lineCap = "round";
+        drawingContext.moveTo(coords.x, coords.y);
         getTouchPosition(event);
-        context.lineTo(coords.x, coords.y);
-        context.stroke();
+        drawingContext.lineTo(coords.x, coords.y);
+        drawingContext.stroke();
     }
+}
+// Stops Painting
+function stopPainting() {paint = false;}
+// Saves the Canvas
+function save() {
+    let imageData;
+    if (!backgroundColorSetting.checked) {
+        imageData = drawingContext.getImageData(0, 0, drawingBoard.width, drawingBoard.height);
+        drawingContext.save();
+        drawingContext.fillStyle = backgroundColor;
+        drawingContext.globalCompositeOperation = "destination-over";
+        drawingContext.fillRect(0, 0, drawingBoard.width, drawingBoard.height);
+        drawingContext.restore();
+    }
+    const documentSelection = document.getElementById("download-options").value;
+    switch (documentSelection) {
+        case "png":
+        case "jpg":
+            saveImage(documentSelection);
+            break;
+        case "pdf":
+            savePDF();
+            break;
+        default:
+            console.error("Unsupported file format selected");
+    }
+    if (!backgroundColorSetting.checked) {
+        drawingContext.save();
+        drawingContext.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+        drawingContext.putImageData(imageData, 0, 0);
+        drawingContext.restore();
+    }
+}
+function saveImage(format) {
+    const dataURL = drawingBoard.toDataURL(`image/${format}`);
+    const link = document.createElement("a");
+    link.href = dataURL;
+    link.download = `signature_${getTimestamp()}.${format}`;
+    link.click();
+}
+const { jsPDF } = window.jspdf;
+function savePDF() {
+    const imgData = drawingBoard.toDataURL("image/jpg", 1.0);
+    const pdf = new jsPDF();
+    const canvasWidth = drawingBoard.width;
+    const canvasHeight = drawingBoard.height;
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.getHeight();
+    const ratio = Math.min(pageWidth / canvasWidth, pageHeight / canvasHeight);
+    const width = canvasWidth * ratio;
+    const height = canvasHeight * ratio;
+    const x = (pageWidth - width) / 2;
+    const y = (pageHeight - height) / 2;
+    pdf.addImage(imgData, 'JPG', x, y, width, height);
+    pdf.save(`signature_${getTimestamp()}.pdf`);
+}
+function getTimestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
 }
